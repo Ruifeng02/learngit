c15d1f0ab9eead49e8614ff85688e13409392f46
Ruifeng <email@example.com>
2018-07-17 12:32:26 +0800

    max min len avg

--- my.js
+++ my.js
@@ -4,14 +4,16 @@ var n=Math.max(6,9,15,-2,92,11)
 		alert("最大值="+n)
 var arr=[6,9,15,-2,92,11]
 		alert("元素数量="+arr.length)
-var arr=[6,9,15,-2,92,11];
-    var sum=0;
-    for (var i = 0; i < arr.length; i++) {
-        sum+=parseInt(arr[i]);
-    };
-    var avg=Math.ceil(sum/arr.length);
-    alert("平均值="+avg)
-			
-					 
-		
+var arr=[6,9,15,-2,92,11]
+function avg(arry){
+var sum=0
+var len
+for(var i=0,len=arry.length;i<len;i++){
+sum+=arry[i]
+}
+var d=sum/len
+d=d.toFixed(2)
+alert("平均值="+d)
+}
+avg(arr)
 		
