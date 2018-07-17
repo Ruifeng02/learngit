5119fab83abb5a9a5583257733d2522a3c44b62f
Ruifeng02 <41247616+Ruifeng02@users.noreply.github.com>
2018-07-17 13:05:04 +0800

    Update my.js

--- my.js
+++ my.js
@@ -1,9 +1,9 @@
 var n=Math.min(6,9,15,-2,92,11)
-		alert("最小值="+n)
+		alert("最小值 = "+n)
 var n=Math.max(6,9,15,-2,92,11)
-		alert("最大值="+n)
+		alert("最大值 = "+n)
 var arr=[6,9,15,-2,92,11]
-		alert("元素数量="+arr.length)
+		alert("元素数量 = "+arr.length)
 var arr=[6,9,15,-2,92,11]
 function avg(arry){
 var sum=0
@@ -13,7 +13,7 @@ sum+=arry[i]
 }
 var d=sum/len
 d=d.toFixed(2)
-alert("平均值="+d)
+alert("平均值 = "+d)
 }
 avg(arr)
 		
