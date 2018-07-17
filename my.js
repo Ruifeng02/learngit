d6a0023a7e5e4955f1c20b403e5c6178bf0d5a2e
Ruifeng <email@example.com>
2018-07-17 09:31:34 +0800

    a js file

---
+++ my.js
@@ -0,0 +1,17 @@
+var n=Math.min(6,9,15,-2,92,11)
+		alert("最小值="+n)
+var n=Math.max(6,9,15,-2,92,11)
+		alert("最大值="+n)
+var arr=[6,9,15,-2,92,11]
+		alert("元素数量="+arr.length)
+var arr=[6,9,15,-2,92,11];
+    var sum=0;
+    for (var i = 0; i < arr.length; i++) {
+        sum+=parseInt(arr[i]);
+    };
+    var avg=Math.ceil(sum/arr.length);
+    alert("平均值="+avg)
+			
+					 
+		
+		
